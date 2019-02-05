746223ac1e16488feb45d9c878fbaac59aceab9f
Foster Stule <camodude5@yahoo.com>
2019-02-04 23:53:21 -0500

    created a login user class controller

---
+++ Chat/ucLogin.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Chat
+{
+	partial class ucLogin
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.lollipopLabel1 = new LollipopLabel();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::Chat.Properties.Resources.squarescube_grande;
+			this.pictureBox1.Location = new System.Drawing.Point(95, 108);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// lollipopLabel1
+			// 
+			this.lollipopLabel1.AutoSize = true;
+			this.lollipopLabel1.BackColor = System.Drawing.Color.Transparent;
+			this.lollipopLabel1.Font = new System.Drawing.Font("Roboto Medium", 10F);
+			this.lollipopLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+			this.lollipopLabel1.Location = new System.Drawing.Point(69, 243);
+			this.lollipopLabel1.Name = "lollipopLabel1";
+			this.lollipopLabel1.Size = new System.Drawing.Size(164, 22);
+			this.lollipopLabel1.TabIndex = 1;
+			this.lollipopLabel1.Text = "lollipopLabel1";
+			// 
+			// ucLogin
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.lollipopLabel1);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "ucLogin";
+			this.Size = new System.Drawing.Size(290, 506);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private LollipopLabel lollipopLabel1;
+	}
+}
