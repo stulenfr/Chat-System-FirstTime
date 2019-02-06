d08dea4fa541a0dd9b517aabcac481044a97db87
Foster Stule <camodude5@yahoo.com>
2019-02-06 18:31:37 -0500

    created a send form

---
+++ Chat/frmSend.cs
@@ -0,0 +1,26 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chat
+{
+	public partial class frmSend : MaterialForm
+	{
+		public frmSend()
+		{
+			InitializeComponent();
+			MaterialSkinManager manager = MaterialSkinManager.Instance;
+			manager.AddFormToManage(this);
+			manager.Theme = MaterialSkinManager.Themes.DARK;
+			manager.ColorScheme = new ColorScheme(Primary.Blue400, Primary.Blue500, Primary.Blue500, Accent.LightGreen400, TextShade.BLACK);
+		}
+	}
+}
