7603f35c3c0fe815f66fd8283b29eb680085eec3
Foster Stule <camodude5@yahoo.com>
2019-02-08 16:43:51 -0500

    altered the design a bit too be more user friendly

--- Chat/ucMain.Designer.cs
+++ Chat/ucMain.Designer.cs
@@ -27,28 +27,29 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.listOnline = new System.Windows.Forms.ListBox();
 			this.SuspendLayout();
 			// 
-			// listBox1
+			// listOnline
 			// 
-			this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.listBox1.FormattingEnabled = true;
-			this.listBox1.ItemHeight = 16;
-			this.listBox1.Items.AddRange(new object[] {
+			this.listOnline.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.listOnline.FormattingEnabled = true;
+			this.listOnline.ItemHeight = 16;
+			this.listOnline.Items.AddRange(new object[] {
             "jack",
             "lucy",
             "tom"});
-			this.listBox1.Location = new System.Drawing.Point(0, 0);
-			this.listBox1.Name = "listBox1";
-			this.listBox1.Size = new System.Drawing.Size(418, 602);
-			this.listBox1.TabIndex = 0;
+			this.listOnline.Location = new System.Drawing.Point(0, 0);
+			this.listOnline.Name = "listOnline";
+			this.listOnline.Size = new System.Drawing.Size(418, 602);
+			this.listOnline.TabIndex = 0;
+			this.listOnline.SelectedIndexChanged += new System.EventHandler(this.listOnline_SelectedIndexChanged);
 			// 
 			// ucMain
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.listOnline);
 			this.Name = "ucMain";
 			this.Size = new System.Drawing.Size(418, 602);
 			this.ResumeLayout(false);
@@ -57,6 +58,6 @@
 
 		#endregion
 
-		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.ListBox listOnline;
 	}
 }
